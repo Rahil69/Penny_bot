f45b67d29df30431bee2568ad0d890efba12716e
Rahil69 <71847528+Rahil69@users.noreply.github.com>
2021-05-14 08:58:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 from urllib.parse import quote_plus
 import discord
+from discord import member
 from discord.client import Client
 from discord.ext import commands
 import random
-import COVID19Py
-#INSTANCES
-covid19 = COVID19Py.COVID19()
-client = commands.Bot(command_prefix='=')
 
-token = ''
+#TOKEN
+client = commands.Bot(command_prefix='=')
+with open('token.txt') as f:
+    contents= f.read()
+token = contents
 
 
 #LOG IN
@@ -16,6 +17,7 @@ token = ''
 async def on_ready():
     print(f'logged in as {client.user}')
     await client.change_presence(activity=discord.Game(name='use  =help | gm'))
+    
 #LATENCY
 @client.command()
 async def ping(ctx):
@@ -40,7 +42,7 @@ async def RetardoMeter(ctx,question):
     if percentnaeesh == 100:
         await ctx.send(f'congratulations {question} {percentnaeesh}% Retarded')
     elif percentnaeesh == 101:
-        await ctx.send(f'wtaf {question} is {percentnaeesh}% Retarded')
+        await ctx.send(f'Bruh. {question} is {percentnaeesh}% Retarded')
     elif percentnaeesh <= 10:
         new_question = str(question)
         upper_question = new_question.upper()
@@ -48,7 +50,7 @@ async def RetardoMeter(ctx,question):
     else:
         await ctx.send(f'{question} is {percentnaeesh}% Retarded')
 
-
+#GET GUILDS 
 @client.command()
 async def getguild(ctx):
     x=0
@@ -57,8 +59,26 @@ async def getguild(ctx):
         guild_list = guild.name
         x+=1
         await ctx.send(f'```{x}) {guild_list}```')
+
+#DELETE MESSAGES
 @client.command()
-async def covidinfo(ctx):
-    info = covid19.getLatest
-    print(info)
+async def clear(ctx,amount=2):
+    await ctx.channel.purge(limit=amount)
+    if True:
+        channel = client.get_channel(ctx.channel.id)
+        embed = discord.Embed(
+            title=f"{ctx.author.name} cleared: {ctx.channel.name}",
+            description=f"{amount} messages were deleted",
+            colour=0x71368a,
+        )
+        await channel.send(embed=embed)
+
+#KICK MEMBERS
+@client.command()
+async def kick(ctx,member:discord.Member,*,reason=None):
+    await member.kick(reason=reason)
+    if True:
+        embedd = discord.Embed(title=f'{member} kicked',description=f'reason: {reason}',color=0x71368a)
+        await ctx.send(embed=embedd)
+
 client.run(token)
